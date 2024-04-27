a049a381654c76f9fb248a65592a2a8a3afffeb4
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-04-27 08:54:45 -0400

    refactored middleware

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import { statusHandler } from "./middleware/status.js";
 
 dotenv.config();
 
@@ -23,12 +24,4 @@ app.listen(port, () => {
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
-});
+app.use(statusHandler);
