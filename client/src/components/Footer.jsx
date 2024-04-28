3e5181d9f0c566eef301ac442be329659a369018
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-04-27 22:48:53 -0400

    Footer UI

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,19 @@
 import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
+import {
+    BsFacebook,
+    BsInstagram,
+    BsTwitterX,
+    BsGithub,
+    BsDribbble,
+} from "react-icons/bs";
 
 const FooterComponent = () => {
     return (
         <Footer container className="border-t-8 border-teal-500">
-            <div>
-                <div>
-                    <div>
+            <div className="w-full max-w-7xl mx-auto">
+                <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+                    <div className="mt-5">
                         <Link
                             to="/"
                             className="
@@ -35,7 +42,7 @@ const FooterComponent = () => {
                             <span className="text-stone-600">Blog</span>
                         </Link>
                     </div>
-                    <div className="grid grid-cols-2 gap-3 mt-4 sm:grid-cols-3 sm:gap-6">
+                    <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
                         <div>
                             <Footer.Title title="About" />
                             <Footer.LinkGroup col>
@@ -67,6 +74,40 @@ const FooterComponent = () => {
                                 </Footer.Link>
                             </Footer.LinkGroup>
                         </div>
+                        <div>
+                            <Footer.Title title="Legal" />
+                            <Footer.LinkGroup col>
+                                <Footer.Link
+                                    href="#"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Privacy
+                                </Footer.Link>
+                                <Footer.Link
+                                    href="#"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Terms &amp; Conditions
+                                </Footer.Link>
+                            </Footer.LinkGroup>
+                        </div>
+                    </div>
+                </div>
+                <Footer.Divider />
+                <div className="w-full sm:flex sm:items-center sm:justify-between">
+                    <Footer.Copyright
+                        href="#"
+                        by="zukkii's blog"
+                        year={new Date().getFullYear()}
+                    />
+                    <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+                        <Footer.Icon href="#" icon={BsFacebook} />
+                        <Footer.Icon href="#" icon={BsInstagram} />
+                        <Footer.Icon href="#" icon={BsTwitterX} />
+                        <Footer.Icon href="#" icon={BsGithub} />
+                        <Footer.Icon href="#" icon={BsDribbble} />
                     </div>
                 </div>
             </div>
