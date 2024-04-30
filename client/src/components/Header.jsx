d23dd1c799e2d392b2d1a600f9f0f4e9a1d6162e
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-04-30 19:28:39 -0400

    theme toggle

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,17 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 const Header = () => {
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
+
     const { currentUser } = useSelector((state) => state.user);
+    const { theme } = useSelector((state) => state.theme);
 
     return (
         <Navbar className="border-b-2">
@@ -52,8 +57,9 @@ const Header = () => {
                     className="w-12 h-10 hidden sm:inline"
                     color="gray"
                     pill
+                    onClick={() => dispatch(toggleTheme())}
                 >
-                    <FaMoon />
+                    {theme === "light" ? <FaMoon /> : <FaSun />}
                 </Button>
                 {currentUser ? (
                     <Dropdown
