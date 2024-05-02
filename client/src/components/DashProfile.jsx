f59e9fa4188785ffe657e1c84806a8a61b6a4efc
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-02 09:51:18 -0400

    upload image functional

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,12 +1,25 @@
-import { Button, TextInput } from "flowbite-react";
-import { useRef, useState } from "react";
+import { Alert, Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+    getStorage,
+    uploadBytesResumable,
+    ref,
+    getDownloadURL,
+} from "firebase/storage";
+
+import { app } from "../firebase";
 
 const DashProfile = () => {
     const { currentUser } = useSelector((state) => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [imageFileUploadingProgress, setImageFileUploadingProgress] =
+        useState(null);
+    const [imageFileUploadError, setImageFileUploadError] = useState(null);
     const filePickerRef = useRef();
+
+    console.log(imageFileUploadingProgress, imageFileUploadError);
 
     const handleImageChange = (e) => {
         const file = e.target.files[0];
@@ -15,8 +28,50 @@ const DashProfile = () => {
             setImageFileUrl(URL.createObjectURL(file)); // set temporary file url
         }
     };
-    // console.log(imageFile);
-    console.log(imageFileUrl);
+
+    useEffect(() => {
+        if (imageFile) {
+            uploadImage();
+        }
+    }, [imageFile]);
+
+    const uploadImage = async () => {
+        /* -- Firebase Storage setup -- */
+        /*
+        service firebase.storage {
+            match /b/{bucket}/o {
+                match /{allPaths=**} {
+                allow read;
+                allow write: if
+                request.resource.size < 2 * 1024 * 1024 &&
+                request.resource.contentType.matches('image/.*')
+                }
+            }
+        }
+        */
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + imageFile.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, imageFile);
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setImageFileUploadingProgress(progress.toFixed(0));
+            },
+            (error) => {
+                setImageFileUploadError(
+                    "Could not upload image (File must be smaller than 2MB)"
+                );
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setImageFileUrl(downloadURL);
+                });
+            }
+        );
+    };
 
     return (
         <div className="max-w-lg mx-auto p-3 w-full">
@@ -48,6 +103,11 @@ const DashProfile = () => {
                         "
                     />
                 </div>
+                {imageFileUploadError && (
+                    <Alert color="failure" className="items-center">
+                        {imageFileUploadError}
+                    </Alert>
+                )}
                 <TextInput
                     type="text"
                     id="username"
