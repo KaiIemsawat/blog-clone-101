8e41e4b30582dbfd89a3f9cdfeac6bd19bbbe69b
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-05 00:27:55 -0400

    signout (modal and header)

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -20,6 +20,7 @@ import {
     deleteUserStart,
     deleteUserSuccess,
     deleteUserFailure,
+    signoutSuccess,
 } from "../redux/user/userSlice";
 
 const DashProfile = () => {
@@ -159,6 +160,23 @@ const DashProfile = () => {
         }
     };
 
+    const handleSignout = async () => {
+        try {
+            const res = await fetch("/api/user/signout", {
+                method: "POST",
+            });
+
+            const data = await res.json();
+            if (!res.ok) {
+                console.log(data.message);
+            } else {
+                dispatch(signoutSuccess());
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     return (
         <div className="max-w-lg mx-auto p-3 w-full">
             <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -251,7 +269,10 @@ const DashProfile = () => {
                 >
                     Delete Account
                 </span>
-                <span className="cursor-pointer hover:text-amber-500 hover:underline duration-300">
+                <span
+                    onClick={handleSignout}
+                    className="cursor-pointer hover:text-amber-500 hover:underline duration-300"
+                >
                     Sign Out
                 </span>
             </div>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 import { toggleTheme } from "../redux/theme/themeSlice";
+import { signoutSuccess } from "../redux/user/userSlice";
 
 const Header = () => {
     const path = useLocation().pathname;
@@ -12,6 +13,23 @@ const Header = () => {
 
     const { currentUser } = useSelector((state) => state.user);
     const { theme } = useSelector((state) => state.theme);
+
+    const handleSignout = async () => {
+        try {
+            const res = await fetch("/api/user/signout", {
+                method: "POST",
+            });
+
+            const data = await res.json();
+            if (!res.ok) {
+                console.log(data.message);
+            } else {
+                dispatch(signoutSuccess());
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
     return (
         <Navbar className="border-b-2">
@@ -85,7 +103,9 @@ const Header = () => {
                             <Dropdown.Item>Profile</Dropdown.Item>
                         </Link>
                         <Dropdown.Divider />
-                        <Dropdown.Item>Sign out</Dropdown.Item>
+                        <Dropdown.Item onClick={handleSignout}>
+                            Sign out
+                        </Dropdown.Item>
                     </Dropdown>
                 ) : (
                     <Link to="/sign-in">
