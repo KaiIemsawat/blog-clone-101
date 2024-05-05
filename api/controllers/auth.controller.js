b8a9ef4d7c7d24f777fee976b4aad25050100a12
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-05 10:17:20 -0400

    admin logic

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -58,7 +58,7 @@ export const signin = async (req, res, next) => {
         }
 
         const token = jwt.sign(
-            { id: validUser._id },
+            { id: validUser._id, isAdmin: validUser.isAdmin },
             process.env.JWT_SECRET_KEY
         );
 
@@ -82,7 +82,7 @@ export const google = async (req, res, next) => {
         const user = await User.findOne({ email });
         if (user) {
             const token = jwt.sign(
-                { id: user._id },
+                { id: user._id, isAdmin: user.isAdmin },
                 process.env.JWT_SECRET_KEY
             );
             const { password, ...rest } = user._doc;
@@ -112,7 +112,7 @@ export const google = async (req, res, next) => {
             });
             await newUser.save();
             const token = jwt.sign(
-                { id: newUser._id },
+                { id: newUser._id, isAdmin: newUser.isAdmin },
                 process.env.JWT_SECRET_KEY
             );
             const { password, ...rest } = newUser._doc;
