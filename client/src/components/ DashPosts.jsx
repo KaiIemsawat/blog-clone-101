b18ac7897839fb538dacd8903feca7ba88521800
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-08 10:01:57 -0400

    base DashPost component. Tailwind scrollbar installed

--- client/src/components/ DashPosts.jsx
+++ client/src/components/ DashPosts.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 const DashPosts = () => {
+    const { currentUser } = useSelector((state) => state.user);
+
+    const [userPosts, setUserPosts] = useState([]);
+    console.log(userPosts);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const res = await fetch(
+                    `/api/post/getPosts?userId=${currentUser._id}`
+                );
+                const data = await res.json();
+                if (res.ok) {
+                    setUserPosts(data.posts);
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        };
+
+        if (currentUser.isAdmin) {
+            fetchPosts();
+        }
+    }, [currentUser._id]);
     return <div> DashPosts</div>;
 };
 export default DashPosts;
