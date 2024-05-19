3171dc734f61450358f98780c76675852b4cf701
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-19 18:59:49 -0400

    homepage, display post cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import { Link } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
 
 const Home = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await fetch("/api/post/getposts");
+            const data = await res.json();
+            setPosts(data.posts);
+        };
+        fetchPosts();
+    }, []);
     return (
         <div>
             <div className="flex flex-col gap-6 p-28  px-8 max-w-6xl mx-auto ">
@@ -24,6 +36,26 @@ const Home = () => {
             </div>
             <div className="p-3 bg-amber-100 dark:bg-slate-700">
                 <CallToAction />
+            </div>
+            <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+                {posts && posts.length > 0 && (
+                    <div className="flex flex-col gap-6">
+                        <h2 className="text-2xl sm:text-3xl font-semibold text-stone-700 dark:text-stone-300">
+                            Recent Posts
+                        </h2>
+                        <div className="flex flex-wrap gap-4">
+                            {posts.map((post) => (
+                                <PostCard key={post._id} post={post} />
+                            ))}
+                        </div>
+                        <Link
+                            to="/search"
+                            className="text-xs sm:text-sm text-amber-500 hover:underline hover:text-blue-600 font-semibold duration-300 text-center sm:text-end sm:pe-4"
+                        >
+                            View all posts
+                        </Link>
+                    </div>
+                )}
             </div>
         </div>
     );
