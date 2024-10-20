bdc1873c0a006ee5997be6823e9ee04cb4becfc3
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-10-20 11:33:02 -0400

    base routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,22 @@
-const App = () => {
-  return <div className="bg-red-500 font-semibold text-green-300">App</div>;
-};
-export default App;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
