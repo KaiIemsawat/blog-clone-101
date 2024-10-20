809607c768240e2139c059b67f0d422db75c6e90
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-10-20 10:58:57 -0400

    boiler plate setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import { store, persistor } from "./redux/store.js";
-import App from "./App.jsx";
-import "./index.css";
-import ThemeProvider from "./components/ThemeProvider.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <PersistGate persistor={persistor}>
-        <Provider store={store}>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </Provider>
-    </PersistGate>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const flowbite = require("flowbite-react/tailwind");
-
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
     },
-    plugins: [flowbite.plugin(), require("tailwind-scrollbar")],
+    plugins: [],
 };
