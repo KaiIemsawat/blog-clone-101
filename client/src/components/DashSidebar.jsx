a6c369e21862fe3cb4839f98f5a1f835030244b2
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-10-26 12:28:00 -0400

    sidebare base

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,39 @@
+import { Sidebar } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+
+export default function DashSidebar() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab"); // get value from query params of ->'tab'
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              label={"User"}
+              labelColor="dark"
+              icon={HiUser}
+              className={`${tab === "profile" && "bg-slate-300 text-stone-700 dark:bg-slate-500"}`}
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item active icon={HiArrowSmRight} className="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
